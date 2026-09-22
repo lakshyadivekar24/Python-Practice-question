139c08db85edd647c9974a9b6ba5d871dade158c
Lakshya Divekar <lakshyadivekar24@gmail.com>
2026-09-22 23:28:50 +0530

    Solved Question 2

---
+++ practice.py
@@ -0,0 +1,11 @@
+##Hello world
+print("hello world")
+
+## wap to check user is eligible for vote or not??
+
+
+age = int(input("Enter Your Age: "))
+if age >=18: 
+    print("You are Eligible for Vote")
+else:
+    print("You are not Eligible for Vote")
