63f8b51ec2cd68e76bc7ae5d1dec67ced8f5df9e
Lakshya Divekar <lakshyadivekar24@gmail.com>
2026-09-22 23:31:05 +0530

    Solved Question 3

--- practice.py
+++ practice.py
@@ -3,9 +3,20 @@ print("hello world")
 
 ## wap to check user is eligible for vote or not??
 
-
+'''
 age = int(input("Enter Your Age: "))
 if age >=18: 
     print("You are Eligible for Vote")
 else:
     print("You are not Eligible for Vote")
+'''
+
+## wap to check no. is +ve or -ve??
+
+num1 = int(input("Enter a Number: "))
+if num1 > 0: 
+    print("The Number is Positive")
+elif num1 < 0: 
+    print("The number is Negative")
+else:
+    print("The Number is Zero")
